f3fc1c067223cc959dd90972b57b679ea7774d65
M Anas <m401anas@gmail.com>
2025-04-23 06:15:54 +0500

    Update With Streaming setup

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
-import chainlit as cl 
-import asyncio
 import os
+import chainlit as cl 
+from openai.types.responses import ResponseTextDeltaEvent
 from agents import Agent, RunConfig, Runner, AsyncOpenAI, OpenAIChatCompletionsModel 
+
 from dotenv import load_dotenv, find_dotenv
 
 # Fetching Api key from .env file
@@ -25,13 +26,13 @@ model = OpenAIChatCompletionsModel(
 # Step 3: Configration At Run Level
 run_config = RunConfig(
     model=model,
-    model_provider = provider,
+    model_provider=provider,
     tracing_disabled=True,
 )
 
 # Step 4:Making Agent
 agent1 = Agent(
-    instructions="You are a helpful assistant.", 
+    instructions="You are a helpful assistant. Your name is bib .You are developed by devloper whose name is MUhammad Anas . He is Agentic first and cloud first developer. ", 
     name="Senior Assitant" 
 )
 
@@ -44,39 +45,53 @@ agent1 = Agent(
 
 # print(result.final_output)
 
-#  With Stream
-async def main():
-    agent=Agent(
-        name="Senior Assitant" ,
-        instructions="You are a helpful assistant."
-    )
 
-    result= Runner.run_streamed(
-        agent1,
-        input="Tell me 5 joke",
-         run_config= run_config,
-    )
-    async for event in result.stream_events():
-        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-            print(event.data.delta,end="",flush=True)
-asyncio.run(main())
 
 @cl.on_chat_start
 async def handel_start_chat():
     cl.user_session.set("history",[])
     await cl.Message(content="Hello, I am a helpful assistant. How can I help you today?").send()
 
+#  With Stream
 
 @cl.on_message
 async def handel_message(message: cl.Message):
     history = cl.user_session.get("history")
+
+    #  Initialize a message object
+    msg = cl.Message(content="")
+    await msg.send()
+
     history.append({"role":"user","content":message.content})
     cl.user_session.set("history",history)
-    result= Runner.run_sync(
+
+    result= Runner.run_streamed(
         agent1,
         input=history,
         run_config= run_config,
     )
+
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            token = event.data.delta
+            await msg.stream_token(token)
+
     history.append({"role":"assistant","content":result.final_output})
     cl.user_session.set("history",history)
-    await cl.Message(content=result.final_output).send()
+    # await cl.Message(content=result.final_output).send()
+
+# async def main():
+#     agent=Agent(
+#         name="Senior Assitant" ,
+#         instructions="You are a helpful assistant."
+#     )
+
+#     result= Runner.run_streamed(
+#         agent1,
+#         input="Tell me 5 joke",
+#          run_config= run_config,
+#     )
+#     async for event in result.stream_events():
+#         if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+#             print(event.data.delta,end="",flush=True)
+# asyncio.run(main())
