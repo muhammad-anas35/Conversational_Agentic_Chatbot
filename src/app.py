b854761c0dc7b8c4e22ae2f45c70d95aa6807f30
M Anas <m401anas@gmail.com>
2025-05-25 11:53:29 +0500

    Adding Live Search in Agent

--- src/app.py
+++ src/app.py
@@ -1,14 +1,26 @@
 import os
-import chainlit as cl 
-from openai.types.responses import ResponseTextDeltaEvent
-from agents import Agent, RunConfig, Runner, AsyncOpenAI, OpenAIChatCompletionsModel 
+import chainlit as cl # type: ignore
+from agents.tool import function_tool # type: ignore
+from openai.types.responses import ResponseTextDeltaEvent # type: ignore 
+from agents import Agent, RunConfig, Runner, AsyncOpenAI, OpenAIChatCompletionsModel # type: ignore 
+from tavily import TavilyClient # type: ignore
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv # type: ignore
 
 # Fetching Api key from .env file
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+search_key =  os.getenv("TAVILY_API_KEY")
 
 load_dotenv(find_dotenv())
+
+# Tool Making
+@function_tool("search_online") # type: ignore
+def search_online(query: str) -> str:
+    """Search the web for answering the given question"""
+    search_client = TavilyClient(api_key= search_key) # type: ignore
+    result = search_client.search(query)
+    return result
+
 
 # Step 1 : Set Provider
 provider = AsyncOpenAI(
@@ -26,31 +38,15 @@ model = OpenAIChatCompletionsModel(
 # Step 3: Configration At Run Level
 run_config = RunConfig(
     model=model,
-    model_provider=provider,
+    model_provider=provider, # type: ignore
     tracing_disabled=True,
 )
-
-# agent1 = Agent(
-#         instructions="You are a helpful assistant.", 
-#         name="Senior Assitant" 
-#     )
-
 # Step 4:Making Agent
 agent1 = Agent(
-    instructions="You are a helpful health assistant. Your name is bib .You are developed by devloper whose name is MUhammad Anas . He is Agentic first and cloud first developer. ", 
-    name="Senior Health Assitant" 
+    name="Senior Health Assitant" ,
+    instructions="You are a helpful health assistant. Your name is bib .You are developed by Muhammad Anas Asif . He is Agentic Ai and Cloud first developer. ", 
+    tools=[search_online],
 )
-
-# Step 5: Run Agent
-# result= Runner.run_sync(
-#     agent1,
-#         input="Who is Founder of PIA?",
-#         run_config= run_config,
-# )
-
-# print(result.final_output)
-
-
 
 @cl.on_chat_start
 async def handel_start_chat():
@@ -85,19 +81,3 @@ async def handel_message(message: cl.Message):
     cl.user_session.set("history",history)
 
     # await cl.Message(content=result.final_output).send()
-
-# async def main():
-#     agent=Agent(
-#         name="Senior Assitant" ,
-#         instructions="You are a helpful assistant."
-#     )
-
-#     result= Runner.run_streamed(
-#         agent1,
-#         input="Tell me 5 joke",
-#          run_config= run_config,
-#     )
-#     async for event in result.stream_events():
-#         if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-#             print(event.data.delta,end="",flush=True)
-# asyncio.run(main())
