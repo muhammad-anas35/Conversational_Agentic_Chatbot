af2bb83882783e4e221ff664a3483dd14c6f217d
muhammad-anas35 <m401anas@gmail.com>
2025-06-01 02:27:16 +0500

    Adding latest and new Feature

--- src/app.py
+++ src/app.py
@@ -1,58 +1,49 @@
-from agents import Agent,RunConfig,Runner,AsyncOpenAI,OpenAIChatCompletionsModel,function_tool,set_tracing_disabled # type: ignore 
-from openai.types.responses import ResponseTextDeltaEvent # type: ignore 
-from agents.extensions.models.litellm_model import LitellmModel # type: ignore
-from tavily import TavilyClient # type: ignore
-import chainlit as cl # type: ignore
+from agents import Agent,Runner,AsyncOpenAI,function_tool,set_tracing_disabled ,set_default_openai_api,set_default_openai_client
+from openai.types.responses import ResponseTextDeltaEvent 
+from tavily import TavilyClient  # type: ignore
+import chainlit as cl 
+from dotenv import load_dotenv, find_dotenv 
 import os
 
-from dotenv import load_dotenv, find_dotenv # type: ignore
+# Configrations
 set_tracing_disabled(disabled=True)
+set_default_openai_api("chat_completions")
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+# gemini_api_key = os.getenv("GEMINI_API_KEY")
+gemini_api_key = "AIzaSyDk00g2Y6vMvDgr3DMjW7557pTBMWdkn9c"
 search_key =  os.getenv("TAVILY_API_KEY")
 
 # Fetching Api key from .env file
 load_dotenv(find_dotenv())
 
 # Tool Making
-@function_tool("search_online") # type: ignore
+@function_tool
 def search_online(query: str) -> str:
     """Search the web for answering the given question"""
     print("Searcing from the web...")
-    search_client = TavilyClient(api_key= search_key) # type: ignore
+    search_client = TavilyClient(api_key= search_key) 
     result = search_client.search(query)
     return result
 
 # # Step 1 : Set Provider
-provider = AsyncOpenAI(
+External_client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
+set_default_openai_client(External_client)
 
-# # Step 2: Set model 
-
-model = OpenAIChatCompletionsModel(
-        model="gemini-2.0-flash",
-        openai_client=provider,
-
-)
-
-# Step 3: Configration At Run Level
-run_config = RunConfig(
-    model=model,
-    model_provider=provider, # type: ignore
-)
-# Step 4:Making Agent
-agent1 = Agent(
+# Step 2:Making Agent
+Main_Agent :Agent= Agent(
     name="Senior Health Assitant" ,
-    instructions="You are a helpful health assistant. Your name is bib .You are developed by Muhammad Anas Asif . He is Agentic Ai and Cloud first developer. ", 
+    instructions="You are a helpful health assistant. Your name is Bib_Ai .You are developed by Muhammad Anas Asif . He is Agentic Ai and Cloud first developer. ", 
+    model="gemini-2.0-flash",
     tools=[search_online],
 )
 
 @cl.on_chat_start
 async def handel_start_chat():
     cl.user_session.set("history",[])
-    await cl.Message(content="Hello, I am a helpful health assistant. How can I help you today?").send()
+    await cl.Message(content="Hi, I am a helpful health assistant. How can I help you today?").send()
 
 #  With Stream
 
@@ -67,10 +58,9 @@ async def handel_message(message: cl.Message):
     history.append({"role":"user","content":message.content})
     cl.user_session.set("history",history)
 
-    result= Runner.run_streamed(
-        agent1,
+    result=Runner.run_streamed(
+        Main_Agent,
         input=history,
-        run_config= run_config,
     )
 
     async for event in result.stream_events():
